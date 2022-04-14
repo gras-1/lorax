4c4a2811ff27d86694e918af369eb78c073fdc27
travis bell <103448884+gras-1@users.noreply.github.com>
2022-04-14 13:04:22 -0500

    minor fixes

--- movement.cs
+++ movement.cs
@@ -1,13 +1,14 @@
 using system;
 using UnityEngine;
 
+
 public float health = 100;
 new public rigidBody2D rb;
 new Vector2 coords;
 public Float movespeed = 3;
 new public Camera main;
 
-public class movement : Monobehavior {
+public class movement : MonoBehavior {
 	
 void FixedUpdate() {
 	
@@ -15,18 +16,17 @@ void FixedUpdate() {
 	float coords.x = Input.GetAxisRaw("Horizontal")
 	
 	transform.position = transform.position + coords(coords.x * movespeed * Time.fixedDeltaTime, coords.y * movespeed * Time.fixedDeltaTime, 0);
-	
+	//moving the cameras y coords
 	if (main.FieldOfViewAxis.Vertical == coords.y){
 		
 		main.transform.position.y = main.transform.position.y * movespeed * Time.FixedDeltaTime;
 		
 			
 	}
+	//moving the cameras x coords
 	if (main.FieldOfViewAxis.Horizontal == coords.x){
 		
 		main.transform.position.x = main.transform.position.x * movespeed * Time.FixedDeltaTime;
-		
-			
 	}
 	
 	
