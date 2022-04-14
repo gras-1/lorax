6e7f17cae138fb739a046f07bacb58ffc12a729a
travis bell <103448884+gras-1@users.noreply.github.com>
2022-04-14 09:58:07 -0500

    added camera movement
    added camera movement once player gets to the edge of the screen ( lines 19 -- 27)

--- movement.cs
+++ movement.cs
@@ -5,6 +5,7 @@ public float health = 100;
 new public rigidBody2D rb;
 new Vector2 coords;
 public Float movespeed = 3;
+new public Camera main;
 
 public class movement : Monobehavior {
 	
@@ -14,6 +15,19 @@ void FixedUpdate() {
 	float coords.x = Input.GetAxisRaw("Horizontal")
 	
 	transform.position = transform.position + coords(coords.x * movespeed * Time.fixedDeltaTime, coords.y * movespeed * Time.fixedDeltaTime, 0);
+	
+	if (main.FieldOfViewAxis.Vertical == coords.y){
+		
+		main.transform.position.y = main.transform.position.y * movespeed * Time.FixedDeltaTime;
+		
+			
+	}
+	if (main.FieldOfViewAxis.Horizontal == coords.x){
+		
+		main.transform.position.x = main.transform.position.x * movespeed * Time.FixedDeltaTime;
+		
+			
+	}
 	
 	
 }
