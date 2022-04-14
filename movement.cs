436ff633cbd73e359bcdbea0330d45d121a21070
travis bell <103448884+gras-1@users.noreply.github.com>
2022-04-14 13:11:57 -0500

    Update movement.cs

--- movement.cs
+++ movement.cs
@@ -16,16 +16,15 @@ void FixedUpdate() {
 	float coords.x = Input.GetAxisRaw("Horizontal")
 	
 	transform.position = transform.position + coords(coords.x * movespeed * Time.fixedDeltaTime, coords.y * movespeed * Time.fixedDeltaTime, 0);
+	
 	//moving the cameras y coords
 	if (main.FieldOfViewAxis.Vertical == coords.y){
-		
 		main.transform.position.y = main.transform.position.y * movespeed * Time.FixedDeltaTime;
 		
 			
 	}
 	//moving the cameras x coords
 	if (main.FieldOfViewAxis.Horizontal == coords.x){
-		
 		main.transform.position.x = main.transform.position.x * movespeed * Time.FixedDeltaTime;
 	}
 	
