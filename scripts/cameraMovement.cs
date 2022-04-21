01c894c3bb874b6c170012b9f7897ff16af77d71
travis bell <103448884+gras-1@users.noreply.github.com>
2022-04-21 13:56:05 -0500

    finished movment
    lines 18 -- 25 and 12 -- 13

--- scripts/cameraMovement.cs
+++ scripts/cameraMovement.cs
@@ -9,8 +9,21 @@ new RigidBody2D rigidbody;
 
 public cameraMovement : MonoBehavour 
 {
+  private float negCamCoordsX = pixelWidth - (pixelWidth * 2);
+  private float negCamCoordsY = pixelHeight - (pixelHeight * 2);
+  
   if (playermove.coords.x == pixelWidth){
     rigidbody.position = rigidbody.position * Time.FIxedDeltaTime * playermove.movespeed;
   }
+  if (playermove.coords.x == negCamCoords){
+    rigidbody.position = rigidbody.position * Time.FIxedDeltaTime * playermove.movespeed;
+  }
+   if (playermove.coords.y == pixelHeight){
+     rigidbody.position = rigidbody.position * Time.FIxedDeltaTime * playermove.movespeed;
+  }
+   if (playermove.coords.y == negCamCoordsY){
+     rigidbody.position = rigidbody.position * Time.FIxedDeltaTime * playermove.movespeed;
+  }
+  
 }
 
