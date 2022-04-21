280ec49619c20663c27956374f09ebecc4e8ac84
travis bell <103448884+gras-1@users.noreply.github.com>
2022-04-21 08:43:43 -0500

    added right movement
    liines(11 -- 14

--- scripts/cameraMovement.cs
+++ scripts/cameraMovement.cs
@@ -1,10 +1,16 @@
 using system;
 using UnityEngine;
+using playermove;
 
-new Vector2 coords;
+new Vector2 camcoords;
+new RigidBody2D rigidbody;
+
+
 
 public cameraMovement : MonoBehavour 
 {
-
+  if (playermove.coords.x == pixelWidth){
+    rigidbody.position = rigidbody.position * Time.FIxedDeltaTime * playermove.movespeed;
+  }
 }
 
